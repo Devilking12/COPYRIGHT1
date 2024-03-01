1f60b8a8e5a156236a944808a275770491e784c7
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-03-01 16:54:21 +0530

    Update main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -40,7 +40,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/7f8ebddf56559ac69d31b.jpg",
+        photo="https://graph.org/file/a3805162e96e2db9c6642.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
@@ -49,7 +49,7 @@ async def start(_, msg):
 gd_buttons = [              
         [
             InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/HEROKUFREECC"),    
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/feelings_for_you_baby"),    
         ]
         ]
 
